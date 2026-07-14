1c718f295efe0e9fd32e8d7f3e27d5e229f84621
you-kou <74287603+you-kou@users.noreply.github.com>
2026-07-14 21:34:15 +0800

    create 常函数

--- src/config.ts
+++ src/config.ts
@@ -50,11 +50,11 @@ export const SITE: SiteConfig = {
   // ==========================================
 
   /** Default site title used as homepage <title> and meta. */
-  title: '拾数记',
+  title: '拾数笔记',
   /** Site tagline / description. */
   description:
     '嗨，我是博客的主人！' +
-    '欢迎来到《拾数记》。这里是我私人的“数学拾荒角落”。' +
+    '欢迎来到《拾数笔记》。这里是我私人的“数学拾荒角落”。' +
     '在生活中，我们常被大数据的概率裹挟，但我更迷恋它最初在赌桌上诞生的那份纯粹；相比于大起大落的算术平均，我更倾向于用几何平均数式的平稳视角来观察这个世界。' +
     '不用正襟危坐，数学本该是一场浪漫的思维冒险。随便逛逛吧！',
   /** Author/handle shown in footer + meta. */
